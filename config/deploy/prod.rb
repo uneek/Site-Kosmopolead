90eab3261021e1832748cee0040fa0c5abe648de
Uneek <favreau.gaelle@gmail.com>
2010-11-16 14:26:51 +0100

    capistrano

---
+++ config/deploy/prod.rb
@@ -0,0 +1,17 @@
+#	Application Settings
+###########################################################
+# ex : kosmopolead-cms
+set :application_fullname, "kosmopolead"
+# ex : Application short name is the directory name : cms (Located in /var/www/products/prod/cms)
+set :application_shortname, "kosmopolead"
+# ex : dev, beta or prod deploy
+set :application_version, "prod"
+# Passenger Rails env
+set :rails_env, "production"
+
+#	Repository Settings
+#########################################################
+# Branch
+set :branch, "master"
+
+eval IO.read(File.join('config', 'deploy', 'config.rb'))
