8ee144807bc4138c30ce7c48f6cc4094e13fb86e
Lucas <uneek@uneek-desktop.(none)>
2010-11-16 14:11:12 +0100

    retrait exts

--- vendor/extensions/home_slider/app/controllers/admin/slides_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::SlidesController < Admin::ResourceController
- model_class Slide
-
-#  def create
-#    @slide = Slide.new( params[:slide] )
-#  end
-
-end
--- vendor/extensions/home_slider/app/models/slide.rb
+++
@@ -1,3 +0,0 @@
-class Slide < ActiveRecord::Base
-   has_attached_file :image, :styles => { :medium =>"315x180>", :thumb => "105x60>"}
-end
