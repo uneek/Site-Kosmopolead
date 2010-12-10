e12f869d62eef61f2d69665625303af3fb215a91
Lucas <uneek@uneek-desktop.(none)>
2010-12-10 12:10:34 +0100

    actus

--- vendor/extensions/actu/app/models/actu_tags.rb
+++ vendor/extensions/actu/app/models/actu_tags.rb
@@ -7,7 +7,7 @@ module ActuTags
 
   tag 'actus:each' do |tag|
     result = []
-    Actu.find(:all, :order => 'id DESC').each do |actu|
+    Actu.find(:all, :order => 'actu_date DESC, id DESC').each do |actu|
       tag.locals.actu = actu
       result << tag.expand
     end
@@ -16,6 +16,6 @@ module ActuTags
 
   tag 'actus:each:actu' do |tag|
     actu = tag.locals.actu
-    %{<li><a href="#{actu.lien || '#'}" target="_blank" ><p><span>#{actu.actu_date}</span><br />#{actu.contenu}</p></a></li>}
+    %{<li><a href="#{actu.lien || '#'}" target="_blank" ><p><span>#{actu.actu_date.strftime('%d-%m-%Y')}</span><br />#{actu.contenu}</p></a></li>}
   end
 end
